20d98bf1da9a60c7caff39cfba513ee400e596d0
Yaroslav Mamrokha <yaroslav.mamrokha@gmail.com>
2020-01-13 16:29:08 +0000

    NPM modules

---
+++ tl_2/js/shop.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer(function(req, res){
+    fs.readFile('index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+}).listen(8000);
